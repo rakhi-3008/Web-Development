a14e083674cf671843de7b5b70f35297bb43cfc4
Rakhi <rakhipanwar819@gmail.com>
2026-04-01 00:21:06 +0530

    try and catch

---
+++ JavaScript/try&catch.js
@@ -0,0 +1,14 @@
+//try - this statement allows you to define a block of code to be tested for errors while it is being executed.
+
+//catch - this statement allows you to define a block of code to be executed, if an error occurs in the try block.
+console.log("hello");
+console.log("hello");
+try{
+    console.log(a);
+}catch(e){
+    console.log(e);
+    console.log("caught an error..a is not defined");
+}
+console.log("hello");
+console.log("hello");
+console.log("hello");
